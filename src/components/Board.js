036d8a6ba8ae774a35a3d70eace25b973773ee33
Vy Nguyen <vy.ntt0120@gmail.com>
2024-08-14 22:45:04 +0700

    submit first code

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Square from "./Square";
+
+export default function Board({ squares, handleClick }) {
+  return (
+    <div className="board">
+      {[0, 1, 2].map((rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {[0, 1, 2].map((colIndex) => (
+            <Square 
+            key={`${rowIndex}${colIndex}`}
+            value= {squares[rowIndex * 3 + colIndex]}
+            handleClick={() => handleClick(rowIndex * 3 + colIndex)} 
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+      )}
